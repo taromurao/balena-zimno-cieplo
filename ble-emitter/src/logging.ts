ba1f0e613d1c339007f5e272294fd8bda968f993
Taro Murao <taro.murao@balena.io>
2021-12-14 17:09:03 +0100

    Show timestamp on log messages.

--- ble-emitter/src/logging.ts
+++ ble-emitter/src/logging.ts
@@ -1,6 +1,6 @@
 import * as winston from 'winston';
 
-const { combine, timestamp, json } = winston.format;
+const { colorize, combine, metadata, timestamp, printf, splat } = winston.format;
 
 export const baseLogger = winston.createLogger({
     level: 'debug',
@@ -8,7 +8,12 @@ export const baseLogger = winston.createLogger({
         new winston.transports.Console()
     ],
     format: combine(
-        json(),
-        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        colorize(),
+        splat(),
+        metadata(),
+        timestamp(),
+        printf(({ timestamp, level, message, metadata }) => {
+            return `[${timestamp}] ${level}: ${message}. ${JSON.stringify(metadata)}`;
+        })
     ),
 });
--- zimnocieplo/src/logging.ts
+++ zimnocieplo/src/logging.ts
@@ -1,13 +1,19 @@
 import * as winston from 'winston';
 
-const { combine, timestamp, json } = winston.format;
+const { colorize, combine, metadata, timestamp, printf, splat } = winston.format;
 
 export const baseLogger = winston.createLogger({
+    level: 'debug',
     transports: [
         new winston.transports.Console()
     ],
     format: combine(
-        json(),
-        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        colorize(),
+        splat(),
+        metadata(),
+        timestamp(),
+        printf(({ timestamp, level, message, metadata }) => {
+            return `[${timestamp}] ${level}: ${message}. ${JSON.stringify(metadata)}`;
+        })
     ),
 });
